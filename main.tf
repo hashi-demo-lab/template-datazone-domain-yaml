beb12ac5570367c0540a9abd9dc1ead9e683da2b
Simon Lynch <simon.lynch@hashicorp.com>
2024-05-13 15:48:44 +1000

    add project members

--- main.tf
+++ main.tf
@@ -25,3 +25,32 @@ module "datazone_domain" {
 
 
 
+
+variable "revision" {
+  default = "1"
+}
+######
+###### This is a workaround due to lack of support in AWS Cloud Control API for managing IAM users
+resource "terraform_data" "aws" {
+  for_each = module.datazone_domain.projects
+  
+  input = var.revision
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOF
+set -e
+CREDENTIALS=(`aws sts assume-role-with-web-identity --role-arn arn:aws:iam::855831148133:role/tfc-tfc-demo-au-retail_data_plaform --role-session-name build-session --web-identity-token $(cat $HOME/tfc-aws-token) --duration-seconds 1000 \
+  --query "[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]" \
+  --output text`)
+
+unset AWS_PROFILE
+export AWS_DEFAULT_REGION=ap-southeast-2
+export AWS_ACCESS_KEY_ID="$${CREDENTIALS[0]}"
+export AWS_SECRET_ACCESS_KEY="$${CREDENTIALS[1]}"
+export AWS_SESSION_TOKEN="$${CREDENTIALS[2]}"
+
+aws datazone create-project-membership --domain-identifier ${each.value.domain_id} --designation PROJECT_OWNER --region ${var.region} --project-identifier ${each.value.project_id} --member '{"userIdentifier":"arn:aws:iam::855831148133:role/aws_simon.lynch_test-developer"}' --output json
+EOF
+  }
+}
